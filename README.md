49478d876d3c677d1e027a98921f8973baf4372e
CodHeK <gaganganapathyas@yahoo.in>
2017-06-24 03:13:48 +0530

    skills bar added

---
+++ README.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# fossiiita.github.io
+Official Website Of FOSS-IIITA<br>
+fossiiita.github.io
+=======
+# CodHeK.io
+>>>>>>> c5f25ad0ed90c9920a1f717734321e63d4198457
---
+++ js/custom.js
@@ -0,0 +1,103 @@
+
+ /* jQuery Pre loader
+  -----------------------------------------------*/
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+});
+
+$(window).scroll(function() {
+  $("#home").hide(1000);
+  $("#skills").fadeIn(1000);
+});
+
+var skillsDiv = jQuery('#skills');
+
+jQuery(window).on('scroll', function() {
+  var winT = jQuery(window).scrollTop(),
+    winH = jQuery(window).height(),
+    skillsT = skillsDiv.offset().top;
+  if (winT + winH > skillsT) {
+    jQuery('.skillbar').each(function() {
+      jQuery(this).find('.skillbar-bar').animate({
+        width: jQuery(this).attr('data-percent')
+      }, 6000);
+    });
+  }
+});
+
+$(document).ready(function() {
+
+  /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+  /* Smoothscroll js
+  -----------------------------------------------*/
+    $(function() {
+        $('.navbar-default a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 49
+            }, 1000);
+            event.preventDefault();
+        });
+    });
+
+
+ /* Home Slideshow Vegas
+  -----------------------------------------------*/
+  $(function() {
+    $('body').vegas({
+        slides: [
+            { src: 'images/slide-img1.jpg' },
+            { src: 'images/slide-img2.jpg' },
+            { src: 'images/slide-img3.jpg' }
+        ],
+        timer: false,
+        transition: [ 'zoomIn', ],
+        animation: ['kenburns']
+    });
+  });
+
+
+  /* Team carousel
+  -----------------------------------------------*/
+  $(document).ready(function() {
+      $("#team-carousel").owlCarousel({
+          items : 3,
+          itemsDesktop : [1199,3],
+          itemsDesktopSmall : [979,3],
+          slideSpeed: 300,
+          itemsDesktop : [1199,2],
+          itemsTablet: [768,1],
+          itemsTabletSmall: [985,2],
+          itemsMobile : [479,1],
+      });
+    });
+    
+
+    /* Back to Top
+    -----------------------------------------------*/
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 200) {
+          $('.go-top').fadeIn(200);
+            } else {
+                $('.go-top').fadeOut(200);
+           }
+        });   
+          // Animate the scroll to top
+        $('.go-top').click(function(event) {
+          event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 300);
+    });
+
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+  });
+
